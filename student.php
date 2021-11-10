2d091dfa594199bdceb8bfb5c0f8502ba0edeaac
tiepcodegym <viettiep122@gmail.com>
2021-11-10 08:51:11 +0700

    capnhat

---
+++ student.php
@@ -0,0 +1,8 @@
+<?php
+$students=array(
+    array('name'=>"Tiep","age"=>18,"address"=>"Ba Tu"),
+    array('name'=>"Luc","age"=>19,"address"=>"Ha Nam"),
+    array('name'=>"Nam","age"=>20,"address"=>"Ha Noi"),
+    array('name'=>"Ha Thu","age"=>21,"address"=>"Phu Tho"),
+    array('name'=>"Dung","age"=>22,"address"=>"Hue"),
+);
